ced3c8a29ba02018d4c89c9e8553570207cbcdc3
John Lai <lai.john.py@gmail.com>
2023-04-14 14:45:49 -0400

    Add nav style

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,25 +1,25 @@
 import Logo from '../../assets/images/jlai-logo-2023.svg';
 import NavItems from './navData';
 import React from 'react';
+import { IoIosColorPalette } from 'react-icons/io';
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="container nav-container">
-        <a href="">
+    <nav className="navbar">
+      <div className="container nav-container flex justify-between items-center">
+        <a href="index.html" className="w-[2.5rem] max-w-full mr-4">
           <img src={Logo} alt="John Lai's Logo" />
         </a>
-        <ul className="nav-menu">
-          {
-            // NavItems is an array of objects
-            NavItems.map((item) => (
-              <li>
-                <a href={item.link}>{item.title}</a>
-              </li>
-            ))
-          }
+        <ul className="nav-menu hidden lg:flex gap-3">
+          {NavItems.map((item) => (
+            <li key={item.id}>
+              <a href={item.link}>{item.title}</a>
+            </li>
+          ))}
         </ul>
-        <button></button>
+        <button id="theme-icon" className="text-white cursor-pointer text-3xl">
+          <IoIosColorPalette />
+        </button>
       </div>
     </nav>
   );
