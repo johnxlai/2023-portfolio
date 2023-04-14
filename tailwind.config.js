2813c287b5831e48b6e4ec89206bc0a61a3a1003
John Lai <lai.john.py@gmail.com>
2023-04-13 22:55:42 -0400

    Setup google font and font awesome

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--primary-color)',
+        secondary: 'var(--secondary-color)',
+      },
+    },
   },
   plugins: [],
 };
