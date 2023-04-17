9828fbb6dd9c22533eb148105d879c608192a98b
John Lai <lai.john.py@gmail.com>
2023-04-17 16:24:34 -0400

    About seciton

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,14 @@ import React from 'react';
 const About = () => {
   return (
     <section id="about">
-      <h3 className="section-heading">About</h3>
-      <button className="btn-primary">test</button>
+      <div className="container mx-auto flex">
+        <div className="md:w-1/2">
+          <h3 className="section-heading">About</h3>
+        </div>
+        <div className="md:w-1/2">
+          <h3 className="section-heading">About</h3>
+        </div>
+      </div>
     </section>
   );
 };
