c5975cc9f3940304c8e179e988079d141d148bcb
John Lai <lai.john.py@gmail.com>
2023-04-17 15:53:09 -0400

    header section

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import HeaderImage from '../../assets/images/jlai-logo-2023.svg';
+import HeaderImage from '../../assets/images/john-lai-profile.jpg';
 import SocialData from './socialData';
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="container header-container">
+    <header className="header mt-[5rem]">
+      <div className="container header-container relative flex flex-col items-center">
         <div className="header-profile">
-          <img src={HeaderImage} alt="" />
+          <img className="mb-5" src={HeaderImage} alt="John Lai" />
         </div>
-        <h2>John Lai</h2>
+        <h2 className="mb-3">John Lai</h2>
         <p>
           I’m a Toronto-based Full Stack Developer, a recent graduate from the
           full time University Of Toronto Bootcamp. As a graduate from Creative
@@ -21,11 +21,11 @@ const Header = () => {
           at Jonas Club Software. At Jonas, I’ve worked on over 200 websites,
           building web content and adjusting layouts using HTML and CSS.
         </p>
-        <div>
-          <a href="#contact" className="">
+        <div className="flex gap-3 my-4">
+          <a href="#contact" className="btn-primary">
             talk
           </a>
-          <a href="#portfolio" className="">
+          <a href="#portfolio" className="btn-primary">
             Portfolio
           </a>
         </div>
