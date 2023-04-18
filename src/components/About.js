a6eb3db1fb0e7dc0105067a96c02102317f89783
John Lai <lai.john.py@gmail.com>
2023-04-17 22:35:46 -0400

    Add download cv

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import CV from '../assets/resume/john-lai-resume.pdf';
+import { MdDownload } from 'react-icons/md';
 
 const About = () => {
   return (
@@ -9,6 +11,16 @@ const About = () => {
         </div>
         <div className="md:w-1/2">
           <h3 className="section-heading">About</h3>
+          <div className="cards"></div>
+          <p>
+            I worked as a Front-End Developer at Carpages.ca where I've worked
+            on over 300 websites converting photoshop, sketch and figma mockup
+            to wordpress site using bootstrap and tailwind.
+          </p>
+          <a className="flex items-center" href={CV} download>
+            <span className="mr-2">Download CV</span>
+            <MdDownload />
+          </a>
         </div>
       </div>
     </section>
