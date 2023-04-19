8e4d1207cc5166f0267e72f4080e69b65d45ce3a
John Lai <lai.john.py@gmail.com>
2023-04-18 23:37:10 -0400

    add react projects

--- src/components/sections/portfolio/Project.js
+++ src/components/sections/portfolio/Project.js
@@ -5,8 +5,25 @@ const Project = ({ project }) => {
     <div>
       <Card className="portfolio">
         <div className="portfolio-image">
-          {project.title}
-          <img src="" alt="" />
+          <img src={project.image} alt={project.title} />
+          <h4>{project.title}</h4>
+          <p>{project.desc}</p>
+          <div className="flex gap-3 mb-3">
+            <a
+              href={project.demo}
+              className="btn-primary block"
+              rel="noopener noreferrer"
+              target="_blank">
+              Demo
+            </a>
+            <a
+              href={project.github}
+              className="btn-primary block"
+              rel="noopener noreferrer"
+              target="_blank">
+              Demo
+            </a>
+          </div>
         </div>
       </Card>
     </div>
