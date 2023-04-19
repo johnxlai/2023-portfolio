0cae2eef44ca74c5ea1629b4fb59dd228966730f
John Lai <lai.john.py@gmail.com>
2023-04-19 15:53:20 -0400

    Add say hello section

--- src/components/sections/contact/Contact.js
+++ src/components/sections/contact/Contact.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import ContactData from './contactData';
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact" className="py-10 px-4">
+      <div className="container mx-auto text-white">
+        <h3 className="section-heading ">SAY HELLO</h3>
+        <p>Let's get in touch, shoot me a message!</p>
+        <div className="contact-container">
+          {ContactData.map((contact) => {
+            return (
+              <a
+                key={contact.id}
+                href={contact.link}
+                target="_blank"
+                rel="noopener noreferrer">
+                {contact.icon}
+                {contact.link}
+              </a>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
