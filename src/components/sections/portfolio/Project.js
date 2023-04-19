65c3e5af424d3e6726c7a3f498f7b751332d524a
John Lai <lai.john.py@gmail.com>
2023-04-18 23:48:00 -0400

    Add portfolio media screen size

--- src/components/sections/portfolio/Project.js
+++ src/components/sections/portfolio/Project.js
@@ -2,31 +2,29 @@ import Card from '../../card/Card';
 
 const Project = ({ project }) => {
   return (
-    <div>
-      <Card className="portfolio">
-        <div className="portfolio-image">
-          <img src={project.image} alt={project.title} />
-          <h4>{project.title}</h4>
-          <p>{project.desc}</p>
-          <div className="flex gap-3 mb-3">
-            <a
-              href={project.demo}
-              className="btn-primary block"
-              rel="noopener noreferrer"
-              target="_blank">
-              Demo
-            </a>
-            <a
-              href={project.github}
-              className="btn-primary block"
-              rel="noopener noreferrer"
-              target="_blank">
-              Demo
-            </a>
-          </div>
+    <Card className="portfolio bg-white rounded p-4 text-purple-800">
+      <div className="portfolio-image">
+        <img className="mb-3" src={project.image} alt={project.title} />
+        <h4>{project.title}</h4>
+        <p>{project.desc}</p>
+        <div className="flex gap-3 mb-3">
+          <a
+            href={project.demo}
+            className="btn-primary block"
+            rel="noopener noreferrer"
+            target="_blank">
+            Demo
+          </a>
+          <a
+            href={project.github}
+            className="bg-purple-600 py-3 rounded p-3 block"
+            rel="noopener noreferrer"
+            target="_blank">
+            Demo
+          </a>
         </div>
-      </Card>
-    </div>
+      </div>
+    </Card>
   );
 };
 
