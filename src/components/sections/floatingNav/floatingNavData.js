9552dd2f638c2f1390b0d7ede559f3dd4adaaa41
John Lai <lai.john.py@gmail.com>
2023-04-19 17:13:31 -0400

    add nav style

--- src/components/sections/floatingNav/floatingNavData.js
+++ src/components/sections/floatingNav/floatingNavData.js
@@ -1,13 +1,14 @@
 import { AiFillHome } from 'react-icons/ai';
+import { BsFillPersonFill } from 'react-icons/bs';
 import { FaSuitcase } from 'react-icons/fa';
 import { TbMessageCircle2Filled } from 'react-icons/tb';
 import { BsFillAwardFill } from 'react-icons/bs';
 
 const data = [
   { id: 1, link: '#', icon: <AiFillHome /> },
-  { id: 2, link: '#about', icon: <AiFillHome /> },
+  { id: 2, link: '#about', icon: <BsFillPersonFill /> },
   { id: 3, link: '#experience', icon: <BsFillAwardFill /> },
-  { id: 4, link: '#projects', icon: <FaSuitcase /> },
+  { id: 4, link: '#portfolio', icon: <FaSuitcase /> },
   { id: 5, link: '#contact', icon: <TbMessageCircle2Filled /> },
 ];
 
--- src/components/sections/navbar/navData.js
+++ src/components/sections/navbar/navData.js
@@ -2,7 +2,7 @@ const data = [
   { id: 1, link: '#', title: 'home' },
   { id: 2, link: '#about', title: 'about' },
   { id: 3, link: '#experience', title: 'experience' },
-  { id: 4, link: '#projects', title: 'projects' },
+  { id: 4, link: '#portfolio', title: 'projects' },
   { id: 5, link: '#contact', title: 'contact' },
 ];
 
