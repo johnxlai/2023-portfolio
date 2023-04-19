031758856a72a54c0b422537ca5e368e2a0c6042
John Lai <lai.john.py@gmail.com>
2023-04-19 15:59:32 -0400

    Add contact info

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Footer = () => {
-  return <footer id="footer">Footer</footer>;
+  return (
+    <footer id="footer" className="bg-blue-900 text-white">
+      <div className="container mx-auto text-center py-5">
+        <h6>© JOHN LAI · All Rights Reserved</h6>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
