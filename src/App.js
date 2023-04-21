1639cff424feec6ffbaddee326db140ec42fb3f2
John Lai <lai.john.py@gmail.com>
2023-04-21 10:19:43 -0400

    Add floating nav logic

--- src/App.js
+++ src/App.js
@@ -9,10 +9,24 @@ import FloatingNav from './components/sections/floatingNav/FloatingNav';
 import Portfolio from './components/sections/portfolio/Portfolio';
 import Services from './components/sections/service/Services';
 import Skills from './components/sections/skills/Skills';
+import { useRef, useState, useEffect } from 'react';
 
 function App() {
+  // For the floating nav, hiding and showing the nav on scroll
+  const mainRef = useRef();
+  const [showFloatingNav, setShowFloatingNav] = useState(true);
+  const [siteYPosition, setSiteYPosition] = useState(0);
+  console.log(mainRef.current.getBoundingClientRect());
+
+  const showFloatingNavHandler = () => {
+    setShowFloatingNav(true);
+  };
+  const hideFloatingNavHandler = () => {
+    setShowFloatingNav(false);
+  };
+
   return (
-    <main>
+    <main ref={mainRef}>
       <Navbar />
       <Header />
       <About />
