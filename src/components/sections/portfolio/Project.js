0f4fe942f15905a593b43897095951770b560e90
John Lai <lai.john.py@gmail.com>
2023-04-25 16:55:19 -0400

    Add first 2 projects to portfolio details

--- src/components/sections/portfolio/Project.js
+++ src/components/sections/portfolio/Project.js
@@ -2,22 +2,29 @@ import Card from '../../card/Card';
 
 const Project = ({ project }) => {
   return (
-    <Card className="portfolio bg-gray-50 rounded p-4 text-secondary text-left">
-      <div className="portfolio-image">
-        <img className="mb-5 rounded" src={project.image} alt={project.title} />
+    <Card className="portfolio bg-gray-50 rounded p-4 text-secondary text-left h-full">
+      <div className="flex flex-col gap-3 h-full">
+        <img className="mb-2 rounded" src={project.image} alt={project.title} />
         <h4 className="text-2xl uppercase">{project.title}</h4>
-        <p className="mb-3">{project.desc}</p>
-        <div className="flex  gap-3 mb-3">
+
+        <h6 className="font-openSans text-slate-500 text-sm leading-8">
+          <span className="bg-tertiary text-white mr-2 py-1 px-2  rounded">
+            Tech Stack
+          </span>
+          {project.techStack}
+        </h6>
+        <p className="mb-2">{project.desc}</p>
+        <div className="flex gap-3 mt-auto">
           <a
             href={project.demo}
-            className="btn-primary rounded-lg font-bold py-1 px-2 md:px-4"
+            className="btn-primary rounded-lg font-bold"
             rel="noopener noreferrer"
             target="_blank">
             Demo
           </a>
           <a
             href={project.github}
-            className="btn-tertiary  rounded-lg font-bold py-1 px-2 md:px-4"
+            className="btn-tertiary rounded-lg font-bold"
             rel="noopener noreferrer"
             target="_blank">
             GitHub
--- src/components/sections/portfolio/portfolioData.js
+++ src/components/sections/portfolio/portfolioData.js
@@ -1,28 +1,33 @@
+import FingerSpell from '../../../assets/images/portfolio/fingerspelling.png';
+import Oracle from '../../../assets/images/portfolio/oracle.png';
+import TravelMe from '../../../assets/images/portfolio/travel-me.png';
 import Image1 from '../../../assets/images/portfolio/travel-me.png';
 
 const PortfolioData = [
   {
     id: 1,
-    category: 'React',
-    image: Image1,
-    title: 'React Portfolio',
-    desc: 'A portfolio website built with React',
-    demo: 'https://react-portfolio-website-1.netlify.app/',
-    github: 'https://github.com/johnxlai/react-portfolio-website',
+    category: 'Full Stack',
+    image: FingerSpell,
+    title: 'FingerSpelling Game',
+    desc: 'An app for learning how to finger spell with American sign language. The user will be prompt with images of signs in alphabet which will make up of words. And user will then insert an answer to try to guess the word that is shown in sign language.​',
+    techStack:
+      'JavaScript, HTML, SCSS, Tailwind, Node, Express, MySql, Heroku, Git',
+    demo: 'https://asl-fingerspelling-game.herokuapp.com/',
+    github: 'https://github.com/johnxlai/asl-fingerspelling-game',
   },
   {
     id: 2,
-    category: 'Front End',
-    image: Image1,
-    title: 'React2 Portfolio',
-    desc: 'A portfolio website built with React',
-    demo: 'https://react-portfolio-website-1.netlify.app/',
-    github: 'https://github.com/johnxlai/react-portfolio-website',
+    category: 'Full Stack',
+    image: Oracle,
+    title: 'Oracle - AI Fortune Teller',
+    desc: 'Oracle is MERN stack project that incorporates OpenAI to make predictions using Tarot Cards. Web Speech Recognition API to receive user voice inputs, as well as Googles Text-to-Speech API to have a computer-generated voice reading out the prediction results.',
+    demo: 'https://oracle-online.herokuapp.com/',
+    github: 'https://github.com/winglovecola/oracle',
   },
   {
     id: 3,
-    category: 'Full Stack',
-    image: Image1,
+    category: 'Front End',
+    image: TravelMe,
     title: 'React Portfolio',
     desc: 'A portfolio website built with React',
     demo: 'https://react-portfolio-website-1.netlify.app/',
