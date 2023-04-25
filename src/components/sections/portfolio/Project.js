fd6ea316d25c16ebfa12f263764d71d9c9d3aa30
John Lai <lai.john.py@gmail.com>
2023-04-25 16:09:58 -0400

    Style portfolio btn

--- src/components/sections/portfolio/Project.js
+++ src/components/sections/portfolio/Project.js
@@ -2,25 +2,25 @@ import Card from '../../card/Card';
 
 const Project = ({ project }) => {
   return (
-    <Card className="portfolio bg-white rounded p-4 text-secondary">
+    <Card className="portfolio bg-gray-50 rounded p-4 text-secondary text-left">
       <div className="portfolio-image">
-        <img className="mb-3" src={project.image} alt={project.title} />
-        <h4 className="text-2xl">{project.title}</h4>
+        <img className="mb-5 rounded" src={project.image} alt={project.title} />
+        <h4 className="text-2xl uppercase">{project.title}</h4>
         <p className="mb-3">{project.desc}</p>
-        <div className="flex gap-3 mb-3">
+        <div className="flex  gap-3 mb-3">
           <a
             href={project.demo}
-            className="btn-primary"
+            className="btn-primary rounded-lg font-bold py-1 px-2 md:px-4"
             rel="noopener noreferrer"
             target="_blank">
             Demo
           </a>
           <a
             href={project.github}
-            className="bg-primary text-secondary py-3 rounded p-3 block"
+            className="btn-tertiary  rounded-lg font-bold py-1 px-2 md:px-4"
             rel="noopener noreferrer"
             target="_blank">
-            Demo
+            GitHub
           </a>
         </div>
       </div>
