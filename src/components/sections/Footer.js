e1f76397566ce9328f092a9fc19f640638b1e4cc
John Lai <lai.john.py@gmail.com>
2023-04-26 16:33:09 -0400

    update footer section

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer
-      id="footer"
-      className="angled-accent before:bg-primary bg-primary text-white">
+    <footer id="footer" className="text-white">
       <div className="container mx-auto text-center py-5">
         <h6>© JOHN LAI · All Rights Reserved</h6>
       </div>
--- src/components/sections/contact/Contact.js
+++ src/components/sections/contact/Contact.js
@@ -5,11 +5,11 @@ const Contact = () => {
   return (
     <section
       id="contact"
-      className="angled-accent py-10 px-4 bg-secondary z-10"
+      className="angled-accent px-4 bg-secondary z-10"
       data-aos="fade-up">
-      <div className="container mx-auto w-1/3 text-tertiary border border-tertiary p-8 relative z-10">
+      <div className="container mx-auto md:w-1/2 lg:w-1/4 text-primary border border-primary p-8 relative z-10">
         <h3 className="section-heading">SAY HELLO</h3>
-        <p>Let's get in touch, shoot me a message!</p>
+        <p className="mb-3">Let's get in touch, shoot me a message!</p>
         <div className="contact-container">
           {ContactData.map((contact) => {
             return (
