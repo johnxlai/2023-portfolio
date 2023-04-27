f60327c7b71007e0a42ee123ee4aa882299d2c03
John Lai <lai.john.py@gmail.com>
2023-04-27 16:51:30 -0400

    Remove cruff

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import Header from './components/sections/header/Header';
 import Navbar from './components/sections/navbar/Navbar';
 import Footer from './components/sections/Footer';
@@ -6,7 +5,6 @@ import Contact from './components/sections/contact/Contact';
 import About from './components/sections/about/About';
 import FloatingNav from './components/sections/floatingNav/FloatingNav';
 import Portfolio from './components/sections/portfolio/Portfolio';
-import Services from './components/sections/service/Services';
 import Skills from './components/sections/skills/Skills';
 import { useRef, useState, useEffect } from 'react';
 
@@ -50,10 +48,8 @@ function App() {
       <Navbar />
       <Header />
       <About />
-      {/* <Services /> */}
       <Portfolio />
       <Skills />
-      {/* <Experience /> */}
       <Contact />
       <Footer />
       {showFloatingNav && <FloatingNav />}
