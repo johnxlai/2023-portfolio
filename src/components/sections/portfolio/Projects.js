81847d6ce8cb2e318fb855c63c16e2832153739b
John Lai <lai.john.py@gmail.com>
2023-05-01 22:56:13 -0400

    Add swiper

--- src/components/sections/portfolio/Projects.js
+++ src/components/sections/portfolio/Projects.js
@@ -1,13 +1,39 @@
 import Project from './Project';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { Pagination } from 'swiper';
 
 const Projects = ({ projects }) => {
   return (
     <div
       className="portfolio-projects grid md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-10 items-start"
       data-aos="fade-up">
-      {projects.map((project) => (
+      {/* {projects.map((project) => (
         <Project key={project.id} project={project} />
-      ))}
+      ))} */}
+
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper">
+        <SwiperSlide>Slide 1 123</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
     </div>
   );
 };
